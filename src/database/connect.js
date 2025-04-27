df75dfe667319a260b58e52fa1e74fdef6607eb8
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-04-27 18:58:42 -0300

    correção mongo

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,10 +1,8 @@
 const mogoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cursonodejs.gb7nkxj.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJS`;
-
 const connectDB = async () => {
   try {
-    await mogoose.connect(uri);
+    await mogoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cursonodejs.gb7nkxj.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJS`);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
